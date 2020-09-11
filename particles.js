8117a425363804a05b111c9b0e093c94d37dee16
testing2984 <71140881+testing2984@users.noreply.github.com>
2020-09-11 18:55:05 +0200

    Update particles.js

--- particles.js
+++ particles.js
@@ -27,13 +27,13 @@ var pJS = function(tag_id, params){
         }
       },
       color: {
-        value: rgb(50, 180, 255)
+        value: '#fff'
       },
       shape: {
         type: 'circle',
         stroke: {
           width: 0,
-          color: rgb(50, 180, 255)
+          color: '#ff0000'
         },
         polygon: {
           nb_sides: 5
@@ -67,7 +67,7 @@ var pJS = function(tag_id, params){
       line_linked: {
         enable: true,
         distance: 100,
-        color: rgb(50, 180, 255),
+        color: '#fff',
         opacity: 1,
         width: 1
       },
